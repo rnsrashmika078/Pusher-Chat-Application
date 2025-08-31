4289577af1a86c824d7d8185a9993c0ff0bf2c8e
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-08-31 17:39:07 +0530

    fix bugs #7

--- src/redux/NotifySlicer.ts
+++ src/redux/NotifySlicer.ts
@@ -1,6 +1,5 @@
 "use client";
 
-import { FriendRequest } from "@/interface/Types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 type MessageType = {
   c_id: string;
@@ -25,14 +24,12 @@ interface SimpleMessage {
 interface Params {
   notify: Notify;
   simpleMessage: SimpleMessage | null;
-  friendRequest: FriendRequest[] | null;
   RequestData: MessageType | null;
 }
 
 const initialState: Params = {
   notify: { message: "", status: "", message_header: "" },
   simpleMessage: null,
-  friendRequest: [],
   RequestData: null,
 };
 
@@ -50,24 +47,16 @@ const nofifySlicer = createSlice({
     ) => {
       state.simpleMessage = action.payload;
     },
-    setFriendRequest: (state, action: PayloadAction<FriendRequest>) => {
-      state.friendRequest?.push(action.payload);
-    },
+
     setRequestData: (state, action: PayloadAction<MessageType | null>) => {
       state.RequestData = action.payload;
     },
-    // removeFriendRequest: (state, action: PayloadAction<{ c_id: string }>) => {
-    //   state.friendRequest =
-    //     state.friendRequest &&
-    //     state.friendRequest.filter((req) => req.c_id !== action.payload.c_id);
-    // },
   },
 });
 
 export const {
   setNotify,
   setSimpleNotification,
-  setFriendRequest,
   setRequestData,
   // removeFriendRequest,
 } = nofifySlicer.actions;
