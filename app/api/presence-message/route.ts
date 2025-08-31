2480e2ca36357629109a48fe5e798d5a9f0fc978
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-08-31 18:50:47 +0530

    fix bugs #10

--- app/api/presence-message/route.ts
+++ app/api/presence-message/route.ts
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import Pusher from "pusher";
-import { getServerSession } from "next-auth";
 import Message from "@/src/server_side/backend/models/Message";
 import Conversation from "@/src/server_side/backend/models/Conversation";
-import { authOptions } from "@/src/lib/auth";
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
@@ -25,11 +23,7 @@ export async function POST(req: Request) {
       status,
       createdAt,
     } = await req.json();
-    const session = await getServerSession(authOptions);
-    if (!session) {
-      console.log("Session issue");
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
+   
     const saveContact = await Conversation.find({
       conversationId,
       userid: senderId,
