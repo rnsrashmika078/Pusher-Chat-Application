f235795540e16f92e7079c7721268d5e0faf234b
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-08-31 17:46:24 +0530

    fix bugs #8

--- app/service/chat/page.tsx
+++ app/service/chat/page.tsx
@@ -1,13 +1,11 @@
+export const dynamic = "force-dynamic";
+
 import ChatLayout from "@/src/compnents/Chat/ChatLayout";
 import React from "react";
 import GetAllUsers from "@/src/server_side/actions/GetAllUsers";
 
 const page = async () => {
-  // const session = await getServerSession(authOptions);
   const allUsers = await GetAllUsers();
-
- 
-
   return (
     <div>
       <ChatLayout allUsers={allUsers} />
