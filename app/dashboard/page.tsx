150f609dabc17f904e5ba3a5df5c7fa843e734c6
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2025-08-31 17:36:41 +0530

    fix bugs #6

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
+import { authOptions } from "@/src/lib/auth";
 import { getServerSession } from "next-auth";
 import React from "react";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 export default async function page() {
   const session = await getServerSession(authOptions);
   return <div className="">{session?.user.name}</div>;
